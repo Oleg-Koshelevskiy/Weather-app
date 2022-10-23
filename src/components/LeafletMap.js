5385aaa83c00fc89129aad0c8e29f24fe790bb68
Oleg <oleg17.k@gmail.com>
2022-10-23 12:20:43 +0300

    fixed map refresh after changing location

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,29 +1,27 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "./LeafletMap.css";
 
 const LeafletMap = (props) => {
   const lat = props.coords.latitude;
   const lon = props.coords.longitude;
 
+  const Recenter = () => {
+    const map = useMap();
+    map.setView([lat, lon]);
+    return (
+      <Marker position={[lat, lon]}>
+        <Popup>{props.city}</Popup>
+      </Marker>
+    );
+  };
+
   return (
     <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-
-//         var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-// });
-
-
-// var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-// });
       />
-      <Marker position={[lat, lon]}>
-        <Popup>{props.city}</Popup>
-      </Marker>
+      <Recenter />
     </MapContainer>
   );
 };
